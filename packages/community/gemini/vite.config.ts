c9715266dfc05d9932a06a94166ac26c25e35bf5
frankwittkampf <fawittkampf@yahoo.com>
2024-12-09 10:49:46 -0800

    chore: auto updates by code pipeline

--- packages/community/gemini/vite.config.ts
+++ packages/community/gemini/vite.config.ts
@@ -2,7 +2,6 @@ import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import { defineConfig } from 'vite';
 
-
 export default defineConfig({
   root: __dirname,
   cacheDir: '../../../node_modules/.vite/packages/community/gemini',
