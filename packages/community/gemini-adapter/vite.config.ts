48a63126f0da41ec7716f9b172e6478d3c38b991
frankwittkampf <fawittkampf@yahoo.com>
2024-12-12 09:23:49 -0800

    chore: disable test

--- packages/community/gemini-adapter/vite.config.ts
+++ packages/community/gemini-adapter/vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
   test: {
     watch: false,
     globals: true,
+    passWithNoTests: true,
     environment: 'node',
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     reporters: ['default'],
